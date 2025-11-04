3f5e2037377682d006734da067a819d14094dd08
Neevash Ramdial (Nash) <mail@neevash.dev>
2025-11-04 15:52:17 -0700

    Unused var

--- plugins/openai/vision_agents/plugins/openai/openai_realtime.py
+++ plugins/openai/vision_agents/plugins/openai/openai_realtime.py
@@ -241,7 +241,6 @@ class Realtime(realtime.Realtime):
             # Handle tool calls from OpenAI realtime
             await self._handle_tool_call_event(event)
         elif et == "response.created":
-            e = ResponseCreatedEvent(**event)
             pass
         elif et == "response.done":
             logger.info("OpenAI response done %s", event)
