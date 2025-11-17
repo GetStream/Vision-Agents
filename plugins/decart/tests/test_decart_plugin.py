d4aec11cb373aa70b423d06c119641f8c6a55141
Neevash Ramdial (Nash) <mail@neevash.dev>
2025-11-17 11:04:46 -0700

    Bootstrap new plugin structure

---
+++ plugins/decart/tests/test_decart_plugin.py
@@ -0,0 +1,16 @@
+import pytest
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class TestDecartPlugin:
+    def test_regular(self):
+        assert True
+
+    # example integration test (run daily on CI)
+    @pytest.mark.integration
+    async def test_simple(self):
+        assert True
+
