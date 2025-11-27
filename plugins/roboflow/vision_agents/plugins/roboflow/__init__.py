978e229fdd9f369b11ff7412ffbf3d1d426e31f0
Daniil Gusev <dangusev92@gmail.com>
2025-11-27 13:19:45 +0100

    Fix missing import

--- plugins/roboflow/vision_agents/plugins/roboflow/__init__.py
+++ plugins/roboflow/vision_agents/plugins/roboflow/__init__.py
@@ -7,11 +7,9 @@ Provides object detection using Roboflow's hosted inference API.
 from .events import DetectionCompletedEvent
 from .roboflow_cloud_processor import RoboflowCloudDetectionProcessor
 from .roboflow_local_processor import RoboflowLocalDetectionProcessor
-from .utils import LocalTrack
 
 __all__ = [
     "RoboflowLocalDetectionProcessor",
     "RoboflowCloudDetectionProcessor",
-    "LocalTrack",
     "DetectionCompletedEvent",
 ]
