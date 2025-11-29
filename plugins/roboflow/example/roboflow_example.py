43eb9196c2b9e7af42575aee0add4cc68d5cad6c
Daniil Gusev <dangusev92@gmail.com>
2025-11-29 17:56:05 +0100

    Update Roboflow example

--- plugins/roboflow/example/roboflow_example.py
+++ plugins/roboflow/example/roboflow_example.py
@@ -18,7 +18,9 @@ async def create_agent(**kwargs) -> Agent:
     agent = Agent(
         edge=getstream.Edge(),  # low latency edge. clients for React, iOS, Android, RN, Flutter etc.
         agent_user=User(name="Sports Commentator", id="agent"),
-        instructions="Shown a video feed of a sports game. You will need to commentate over the game and describe the action as a seasoned sports commentator. Only call out actions your are 100% sure about, otherwise just describe what you see in the video feed.",
+        instructions="You will be shown a video feed of a sports game. "
+                     "You will need to commentate over the game and describe the action as a seasoned sports commentator. "
+                     "Only call out actions your are 100% sure about, otherwise just describe what you see in the video feed.",
         processors=[
             roboflow.RoboflowLocalDetectionProcessor(
                 classes=["person", "sports ball"],
@@ -29,6 +31,11 @@ async def create_agent(**kwargs) -> Agent:
         llm=llm,
     )
 
+    @agent.events.subscribe
+    async def on_detection_completed(event: roboflow.DetectionCompletedEvent):
+        # React on detected objects here
+        ...
+
     return agent
 
 
@@ -37,7 +44,6 @@ async def join_call(agent: Agent, call_type: str, call_id: str, **kwargs) -> Non
 
     # Have the agent join the call/room
     with await agent.join(call):
-        await asyncio.sleep(5)
         # run till the call ends
         await agent.finish()
 
