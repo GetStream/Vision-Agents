96ccff474dd2d11de656b0099be2085b89b7e05c
Thierry Schellenbach <thierry@getstream.io>
2026-01-16 12:29:55 -0700

    first draft of the xai realtime integration

--- plugins/xai/vision_agents/plugins/xai/__init__.py
+++ plugins/xai/vision_agents/plugins/xai/__init__.py
@@ -1,4 +1,5 @@
 from .llm import XAILLM as LLM
+from .xai_realtime import XAIRealtime as Realtime
 from .version import __version__
 
-__all__ = ["LLM", "__version__"]
+__all__ = ["LLM", "Realtime", "__version__"]
