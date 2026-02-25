e14b8cf6846061b8a1d9d8b79331cd093cdc5852
Max Kahan <max.kahan@getstream.io>
2026-02-25 20:52:08 +0000

    add HF detection processor

--- plugins/huggingface/vision_agents/plugins/huggingface/events.py
+++ plugins/huggingface/vision_agents/plugins/huggingface/events.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass, field
 from typing import Any, Optional
 
-from vision_agents.core.events import PluginBaseEvent
+from vision_agents.core.events import (
+    DetectedObject,
+    PluginBaseEvent,
+    VideoProcessorDetectionEvent,
+)
 
 
 @dataclass
@@ -20,3 +24,22 @@ class LLMErrorEvent(PluginBaseEvent):
     type: str = field(default="plugin.llm.error", init=False)
     error_message: Optional[str] = None
     event_data: Optional[Any] = None
+
+
+@dataclass
+class DetectionCompletedEvent(VideoProcessorDetectionEvent):
+    """Event emitted when Transformers object detection completes.
+
+    Attributes:
+        objects: Detected objects with labels and bounding boxes.
+        image_width: Width of the source image in pixels.
+        image_height: Height of the source image in pixels.
+    """
+
+    objects: list[DetectedObject] = field(default_factory=list)
+    image_width: int = 0
+    image_height: int = 0
+    type: str = field(default="plugin.huggingface.detection_completed", init=False)
+
+    def __post_init__(self):
+        self.detection_count = len(self.objects)
