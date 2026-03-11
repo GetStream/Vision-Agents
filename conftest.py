ad83c7d25d3202155cfdffdb50bb2720ed26b39f
Daniil Gusev <dangusev92@gmail.com>
2026-03-11 15:18:23 +0100

    Fix blockbuster yelling at redis tests
    Added some exceptions to `blockbuster` config for `redis`. It reads package metadata to get the current version.

--- conftest.py
+++ conftest.py
@@ -67,6 +67,9 @@ def blockbuster(request) -> Iterator[BlockBuster | None]:
         with blockbuster_ctx() as bb:
             for func in bb.functions.values():
                 func.can_block_in(agent_cls_file, "__init__")
+                # Some libs use "importlib_metadata" to determine the current version
+                func.can_block_in("importlib_metadata/__init__.py", "mtime")
+                func.can_block_in("importlib_metadata/__init__.py", "version")
 
             # Allow Python's standard logging which is inherently synchronous.
             if "io.TextIOWrapper.write" in bb.functions:
