70f44be70e7833ff5b7ca2ed7e7814aad4e87b09
Daniil Gusev <dangusev92@gmail.com>
2026-05-09 00:16:25 +0200

    inference.audio: add __all__

--- agents-core/vision_agents/core/agents/inference/__init__.py
+++ agents-core/vision_agents/core/agents/inference/__init__.py
@@ -1,18 +1,4 @@
-from .audio import (
-    AudioInputChunk as AudioInputChunk,
-)
-from .audio import (
-    AudioInputStream as AudioInputStream,
-)
-from .audio import (
-    AudioOutputChunk as AudioOutputChunk,
-)
-from .audio import (
-    AudioOutputFlush as AudioOutputFlush,
-)
-from .audio import (
-    AudioOutputStream as AudioOutputStream,
-)
+from .audio import *
 from .base import InferenceFlow as InferenceFlow
 from .realtime_flow import (
     RealtimeInferenceFlow as RealtimeInferenceFlow,
