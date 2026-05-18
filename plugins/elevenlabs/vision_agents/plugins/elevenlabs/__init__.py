ee9fbf4fdf932bbac8b4eb44068919e07f9bfefa
Daniil Gusev <dangusev92@gmail.com>
2026-05-18 17:55:41 +0200

    Fixes for elevenlabs.STT (#559)
    * Fixes for elevenlabs.STT
    
    - Renamed "STT" -> "ElevenlabsSTT" to simplify code search
    - Fixed duplicated TurnEnded event
    - Fixed keep-alive mechanism. The websocket was getting closed after a few seconds without audio. Now, it sends 2s of silence every 5s to keep the connection open
    
    * ElevenlabsSTT: logger.error -> logger.exception
    
    * ElevenlabsSTT: fix math in silence generation

--- plugins/elevenlabs/vision_agents/plugins/elevenlabs/__init__.py
+++ plugins/elevenlabs/vision_agents/plugins/elevenlabs/__init__.py
@@ -1,7 +1,4 @@
+from .stt import ElevenlabsSTT as STT
 from .tts import TTS
-from .stt import STT
-
-# Re-export under the new namespace for convenience
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 __all__ = ["TTS", "STT"]
